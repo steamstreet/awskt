d6a4547034b955431957bd01071bf3918e02261b
Jon Nichols <jnichols@steamstreet.com>
2023-03-08 13:08:17 -0800

    Fix the cache path.

--- .space.kts
+++ .space.kts
@@ -1,12 +1,12 @@
 job("publish-1.0") {
     container("openjdk:11") {
         kotlinScript {
-            it.gradlew("hello", "-PBUILD_NUMBER=${it.executionNumber()}")
+            it.gradlew("publish", "-PBUILD_NUMBER=${it.executionNumber()}")
         }
 
         cache {
             storeKey = "gradle-{{ hashFiles('gradle/wrapper/gradle-wrapper.properties') }}"
-            localPath = "~/.gradle"
+            localPath = "/root/.gradle/wrapper"
         }
     }
 }
