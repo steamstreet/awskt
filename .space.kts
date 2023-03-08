966819c4642d078ccc0f760a119453e93da06f9e
Jon Nichols <jnichols@steamstreet.com>
2023-03-08 13:14:34 -0800

    Add the .m2 dir to the cache.

--- .space.kts
+++ .space.kts
@@ -8,5 +8,9 @@ job("publish-1.0") {
             storeKey = "gradle-{{ hashFiles('gradle/wrapper/gradle-wrapper.properties') }}"
             localPath = "/root/.gradle/wrapper"
         }
+        cache {
+            storeKey = "m2-{{ hashFiles('build.gradle.kts', 'env/build.gradle.kts', 'settings.gradle.kts') }}"
+            localPath = "/root/.m2/repository"
+        }
     }
 }
