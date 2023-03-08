a3ee480aac93b7e492d4c7bb614e4888ce283016
Jon Nichols <jnichols@steamstreet.com>
2023-03-08 13:34:19 -0800

    Use root/.gradle as the root of the cache.

--- .space.kts
+++ .space.kts
@@ -7,8 +7,8 @@ job("publish-1.0") {
         }
 
         cache {
-            storeKey = "gradle-{{ hashFiles('gradle/wrapper/gradle-wrapper.properties') }}"
-            localPath = "/root/.gradle/wrapper"
+            storeKey = "gradle-root-{{ hashFiles('gradle/wrapper/gradle-wrapper.properties') }}"
+            localPath = "/root/.gradle"
         }
         cache {
             storeKey = "m2-{{ hashFiles('build.gradle.kts', 'env/build.gradle.kts', 'settings.gradle.kts') }}"
