ee1dafd6a40d3f09fb1acb11d0bb008cb79f8c30
Jon Nichols <jnichols@steamstreet.com>
2023-03-08 12:31:52 -0800

    Print the init.gradle file.

--- .space.kts
+++ .space.kts
@@ -1,12 +1,16 @@
+import java.io.File
+
 job("publish-1.0") {
     container("openjdk:11") {
         kotlinScript {
+            val initGradle = File("/mnt/space/system/gradle/init.gradle").readText()
+            println(initGradle)
             it.gradlew("publish", "-PBUILD_NUMBER=${it.executionNumber()}")
         }
 
         cache {
             storeKey = "gradle-{{ hashFiles('gradle/wrapper/gradle-wrapper.properties') }}"
-            localPath = System.getenv("GRADLE_USER_HOME")
+            localPath = "~/.gradle"
         }
     }
 }
