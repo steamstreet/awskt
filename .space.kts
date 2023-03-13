a2fe42c4f45a36f58f131fa5f1453d0b8a921801
Jon Nichols <jnichols@steamstreet.com>
2023-03-12 22:58:39 -0700

    Add the entire gradle config to the cache, along with the chmod.

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@ job("publish-1.0") {
     container("openjdk:11") {
         mountDir = "/root"
 
-        args("chmod", "+x", "/root/.gradle/nodejs/node-v16.13.0-linux-x64/bin/node")
+        args("chmod", "-f", "+x", "/root/.gradle/nodejs/node-v16.13.0-linux-x64/bin/node")
 
         kotlinScript {
             it.gradlew("publish", "-PBUILD_NUMBER=${it.executionNumber()}", "--stacktrace")
@@ -10,16 +10,8 @@ job("publish-1.0") {
 
         val buildFiles = "{{ hashFiles('gradle/wrapper/gradle-wrapper.properties', 'build.gradle.kts', 'env/build.gradle.kts', 'settings.gradle.kts') }}"
         cache {
-            storeKey = "gradle-wrapper-${buildFiles}"
-            localPath = "/root/.gradle/wrapper"
-        }
-        cache {
-            storeKey = "gradle-cache-${buildFiles}"
-            localPath = "/root/.gradle/caches"
-        }
-        cache {
-            storeKey = "gradle-daemons-${buildFiles}"
-            localPath = "/root/.gradle/daemon"
+            storeKey = "gradle-config-${buildFiles}"
+            localPath = "/root/.gradle"
         }
     }
 }
