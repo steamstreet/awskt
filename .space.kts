0f4c468265b8bf08c72ace1c57815a259714d7a0
Jon Nichols <jnichols@steamstreet.com>
2023-04-02 12:07:35 -0700

    Removing caching.

--- .space.kts
+++ .space.kts
@@ -1,17 +1,15 @@
 job("publish-1.0") {
     container("openjdk:11") {
-        mountDir = "/root"
-
-        args("chmod", "-f", "+x", "/root/.gradle/nodejs/node-v16.13.0-linux-x64/bin/node")
+//        mountDir = "/root"
 
         kotlinScript {
             it.gradlew("publish", "-PBUILD_NUMBER=${it.executionNumber()}", "--stacktrace")
         }
-
-        val buildFiles = "{{ hashFiles('gradle/wrapper/gradle-wrapper.properties', 'build.gradle.kts', 'env/build.gradle.kts', 'settings.gradle.kts') }}"
-        cache {
-            storeKey = "gradle-config-${buildFiles}"
-            localPath = "/root/.gradle"
-        }
+//
+//        val buildFiles = "{{ hashFiles('gradle/wrapper/gradle-wrapper.properties', 'build.gradle.kts', 'env/build.gradle.kts', 'settings.gradle.kts') }}"
+//        cache {
+//            storeKey = "gradle-config-${buildFiles}"
+//            localPath = "/root/.gradle"
+//        }
     }
 }
