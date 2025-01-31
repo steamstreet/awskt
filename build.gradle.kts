4516c72e87756fbe7990604dbe3d20f9671ed213
Jon Nichols <jon.nichols@siriusxm.com>
2025-01-31 15:10:57 -0800

    Complete the release process by updating dependencies and passing sonatype information.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ tasks.named("snapshot") {
 }
 
 tasks.named("final") {
-    dependsOn(subprojects.flatMap { it.tasks.matching { it.name == "publishToMavenLocal" } })
-//    dependsOn(subprojects.flatMap { it.tasks.matching { it.name == "publishToSonatype" } })
-//    dependsOn(subprojects.flatMap { it.tasks.matching { it.name == "closeAndReleaseSonatypeStagingRepository" } })
+//    dependsOn(subprojects.flatMap { it.tasks.matching { it.name == "publishToMavenLocal" } })
+    dependsOn(subprojects.flatMap { it.tasks.matching { it.name == "publishToSonatype" } })
+    dependsOn(subprojects.flatMap { it.tasks.matching { it.name == "closeAndReleaseSonatypeStagingRepository" } })
 }
