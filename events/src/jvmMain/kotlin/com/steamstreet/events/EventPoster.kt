2f22cff24dee02df62eaa8560ebc94126a4ad7ff
Jon Nichols <jon.nichols@siriusxm.com>
2025-11-11 10:19:07 -0800

    Add versions of the Event post call that take an ApplicationEventPoster instead of using a global.

--- events/src/jvmMain/kotlin/com/steamstreet/events/EventPoster.kt
+++ events/src/jvmMain/kotlin/com/steamstreet/events/EventPoster.kt
@@ -3,32 +3,6 @@ package com.steamstreet.events
 import com.steamstreet.env.Env
 import com.steamstreet.mutableLazy
 
-/**
- * Defines an interface for submitting application events
- */
-public interface ApplicationEventPoster {
-    /**
-     * Post the event as a string
-     * @return the event id
-     */
-    public suspend fun post(eventType: String, eventDetail: String, source: String? = null): String?
-
-    /**
-     * Post a set of events
-     * @return a list of event ids. For those that are null, the event was not published.
-     */
-    public suspend fun post(events: Collection<Event>): List<String?>
-}
-
-/**
- * Mostly internal representation of an event.
- */
-public interface Event {
-    public val type: String
-    public val detail: String?
-    public val source: String?
-}
-
 public var poster: ApplicationEventPoster by mutableLazy {
     EventBridgeSubmitter(
         Env.optional("EventBusArn") ?: throw IllegalStateException("Missing EventBusArn"),
--- events/src/jvmMain/kotlin/com/steamstreet/events/EventSchemaJvm.kt
+++ events/src/jvmMain/kotlin/com/steamstreet/events/EventSchemaJvm.kt
@@ -5,20 +5,28 @@ import kotlinx.serialization.json.jsonObject
 
 /**
  * Send events from this schema
+ * @param eventPoster the poster to use, defaults to the global event poster.
  */
-public suspend fun <T> EventSchema<T>.post(input: T, source: String? = null): String? {
+public suspend fun <T> EventSchema<T>.post(
+    input: T, source: String? = null,
+    eventPoster: ApplicationEventPoster = poster
+): String? {
     val obj = Json.encodeToJsonElement(this.serializer, input).jsonObject
-    return poster.post(this.type, obj.toString(), source)
+    return eventPoster.post(this.type, obj.toString(), source)
 }
 
 /**
  * Post more than one event.
+ * @param eventPoster the poster to use, defaults to the global event poster.
  */
-public suspend fun <T> EventSchema<T>.post(input: Collection<T>, source: String? = null): List<String?> {
+public suspend fun <T> EventSchema<T>.post(
+    input: Collection<T>, source: String? = null,
+    eventPoster: ApplicationEventPoster = poster
+): List<String?> {
     val events = input.map {
         EventSchemaEvent(this, it, source)
     }
-    return poster.post(events)
+    return eventPoster.post(events)
 }
 
 internal class EventSchemaEvent<T>(val schema: EventSchema<T>, payload: T, override val source: String? = null) :
@@ -27,4 +35,27 @@ internal class EventSchemaEvent<T>(val schema: EventSchema<T>, payload: T, overr
     override val detail: String? by lazy {
         eventSchemaJson.encodeToString(schema.serializer, payload)
     }
+}
+
+/**
+ * Post an event using the ApplicationEventPoster.
+ */
+public suspend fun <T> ApplicationEventPoster.post(schema: EventSchema<T>, input: T, source: String? = null): String? {
+    val obj = Json.encodeToJsonElement(schema.serializer, input).jsonObject
+    return post(schema.type, obj.toString(), source)
+}
+
+
+/**
+ * Post more than one event using an ApplicationEventPoster.
+ */
+public suspend fun <T> ApplicationEventPoster.post(
+    schema: EventSchema<T>,
+    input: Collection<T>,
+    source: String? = null
+): List<String?> {
+    val events = input.map {
+        EventSchemaEvent(schema, it, source)
+    }
+    return post(events)
 }
